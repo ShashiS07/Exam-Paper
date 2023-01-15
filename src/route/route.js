3d276b912860204d462766b191fdeca278e1820a
ShashiS07 <shashiss0708@gmail.com>
2023-01-16 01:20:41 +0530

    update

--- src/route/route.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-const router=express.Router()
-
-router.all('/*',function(req,res){
-    return res.status(404).send({messgae:"Invalid http request"})
-})
-
-module.exports=router
